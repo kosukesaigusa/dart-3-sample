2aa3996b39fbd1d363e0b94040ebf31cee105b26
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2023-05-21 14:41:57 +0900

    feat: add switch expressions samples

---
+++ lib/switch_expressions.dart
@@ -0,0 +1,37 @@
+enum Country {
+  japan,
+  usa,
+  uk,
+  france,
+  ;
+
+  (String, double, double) get capital {
+    // In Dart 2:
+    //
+    // switch (this) {
+    //   case japan:
+    //     return ('Tokyo', 35.681236, 139.767125);
+    //   case usa:
+    //     return ('New York', 40.7128, 74.0060);
+    //   case uk:
+    //     return ('London', 51.5072, 0.1276);
+    //   case france:
+    //     return ('Paris', 48.8566, 2.3522);
+    // }
+
+    return switch (this) {
+      japan => ('Tokyo', 35.681236, 139.767125),
+      usa => ('New York', 40.7128, 74.0060),
+      uk => ('London', 51.5072, 0.1276),
+      france => ('Paris', 48.8566, 2.3522),
+    };
+  }
+
+  String get continent {
+    return switch (this) {
+      japan => 'Asia',
+      usa => 'North America',
+      uk || france => 'Europe',
+    };
+  }
+}
---
+++ test/switch_expressions.dart
@@ -0,0 +1,15 @@
+import 'package:dart_3_sample/switch_expressions.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('Switch Expressions feature sample', () {
+    final capitalOfJapan = Country.japan.capital;
+    expect(capitalOfJapan.$1, 'Tokyo');
+    expect(capitalOfJapan.$2, 35.681236);
+    expect(capitalOfJapan.$3, 139.767125);
+
+    expect(Country.japan.continent, 'Asia');
+    expect(Country.uk.continent, 'Europe');
+    expect(Country.france.continent, 'Europe');
+  });
+}
