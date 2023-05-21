db028f9c69fd844673bfd1eec4235f0cc905aabd
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2023-05-21 14:19:33 +0900

    chore: update bin and allow to use print

--- bin/dart_3_sample.dart
+++ bin/dart_3_sample.dart
@@ -1,5 +1,3 @@
-import 'package:dart_3_sample/dart_3_sample.dart' as dart_3_sample;
-
-void main(List<String> arguments) {
-  print('Hello world: ${dart_3_sample.calculate()}!');
+void main() {
+  print('Hello world!');
 }
